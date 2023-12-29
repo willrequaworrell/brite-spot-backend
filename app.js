76465e5e043be7d6eca62e8ba518adaf2a288145
Will <willrequaworrell@gmail.com>
2023-12-28 19:00:24 -0500

    connected to RDS mysql database using prisma, now operational with basic post routes for users and entries

--- app.js
+++ app.js
@@ -1,11 +1,65 @@
 const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+
 const app = express();
+app.use(express.json());
+
+const prisma = new PrismaClient();
 
 const PORT = process.env.PORT || 3000;
 
 
 app.get("/", (req, res) => {
     res.send('Hello World!')
+})
+
+app.post("/user", async (req, res) => {
+    try {
+        const {email, first_name, password} = req.body
+        
+        const newUser = await prisma.user.create({
+            data: {
+                email, 
+                first_name, 
+                password,
+            }
+        })
+        res.json({
+            "status": "success",
+            "user": newUser
+        })
+        
+    } catch (e) {
+        res.status(500).json({'error': e.message})
+    }
+
+})
+
+app.post("/entry", async (req, res) => {
+    const {userId, content} = req.body
+
+    try {
+        const userExists = await prisma.user.findUnique({
+            where: {id: userId}
+        })
+
+        if (!userExists) {
+            return res.status(404).json({"error": "user not found" })
+        }
+
+        const newEntry = await prisma.entry.create({
+            data: {
+                content, 
+                user :{
+                    connect: {id: userId}
+                }
+            }
+        })
+
+        res.json({"status": "success", "newEntry": newEntry})
+    } catch (e) {
+        res.status(500).json(e.message)
+    }
 })
 
 app.listen(PORT, () => {
---
+++ prisma/migrations/20231228233020_init_models/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `first_name` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `photo_url` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Entry` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `content` VARCHAR(191) NOT NULL,
+    `date` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `userId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Entry` ADD CONSTRAINT `Entry_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
